d029db5c0af15434feddfc511367d3d88e97802d
Shawn Lewis <shlewis@gmail.com>
2011-12-15 21:20:28 -0800

    Basic backend API working.

--- lassoem.py
+++ lassoem.py
@@ -1,43 +1,97 @@
+import hashlib
 import json
+import os
+import sys
+import traceback
+import webob
+import webob.exc
+import webapp2
 
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
 
 import models
 
 
-class MainPage(webapp.RequestHandler):
+class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('Hello, webapp World!')
 
+def json_group(group):
+    return {
+        'name': group.name,
+        'key': str(group.key()),
+        'edit_hash': group.edit_hash,
+        'id': str(group.key().id()),
+        'item_set': [json_item(i) for i in group.item_set],
+        'ordering': group.ordering
+    }
 
-class APIGroup(webapp.RequestHandler):
-    def get(self, key):
-        # list group
-        pass
+def json_item(item):
+    return {
+        'id': str(item.key().id()),
+        'title': item.title,
+        'url': item.url
+    }
+
+class JSONRequestHandler(webapp2.RequestHandler):
+    def json_response(self, obj):
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(obj, indent=4))
+        
+    def handle_exception(self, ex, debug):
+        lines = ''.join(traceback.format_exception(*sys.exc_info()))
+        lines = lines.replace('\n', '<br>')
+        self.request.accept = ''
+        resp = webob.exc.HTTPInternalServerError(detail='%s' % lines)
+        resp.content_type = 'text/plain'
+        return resp
+
+
+class APIGroup(JSONRequestHandler):
+    def get(self, id_):
+        group = models.Group.get_by_id(int(id_))
+        self.json_response(json_group(group))
 
     def post(self):
-        self.response.out.write('response')
-        pass
+        params = json.loads(self.request.body)
 
-    def put(self, key):
+        group = models.Group()
+        setattr(group, 'name', params['name'])
+        edit_hash = hashlib.md5()
+        edit_hash.update(os.urandom(1000))
+        group.edit_hash = edit_hash.hexdigest()
+        group.put()
+
+        self.json_response(json_group(group))
+
+    def put(self, id_):
         # edit group
         pass
 
-    def delete(self, key):
+    def delete(self, id_):
         # delete group
         pass
 
-
-class APIItem(webapp.RequestHandler):
-    def get(self, key):
-        # list item
-        pass
+class APIItem(JSONRequestHandler):
+    def get(self, id_):
+        item = models.Item.get_by_id(int(id_))
+        self.json_response(json_item(item))
 
     def post(self):
-        # create item
-        pass
+        params = json.loads(self.request.body)
+
+        group = db.get(db.Key(params['group']))
+        if group.edit_hash != params['edit_hash']:
+            raise webob.exc.HTTPUnauthorized
+
+        item = models.Item()
+        item.title = params['title']
+        item.url = params['url']
+        item.group = group
+        item.put()
+
+        self.json_response(json_item(item))
 
     def put(self, key):
         # edit item
@@ -47,8 +101,7 @@ class APIItem(webapp.RequestHandler):
         # delete item
         pass
 
-
-application = webapp.WSGIApplication(
+app = webapp2.WSGIApplication(
     [
     ('/', MainPage),
 
@@ -60,9 +113,3 @@ application = webapp.WSGIApplication(
 
     ],
     debug=True)
-
-def main():
-    run_wsgi_app(application)
-
-if __name__ == "__main__":
-    main()
--- models.py
+++ models.py
@@ -2,9 +2,10 @@ from google.appengine.ext import db
 
 class Group(db.Model):
     name = db.StringProperty()
-    num_views = db.IntegerProperty()
-    num_unique_views = db.IntegerProperty()
+    num_views = db.IntegerProperty(default=0)
+    num_unique_views = db.IntegerProperty(default=0)
     edit_hash = db.StringProperty()
+    ordering = db.ListProperty(int)
 
     
 class Item(db.Model):
